bffc5c90285fa0cf8319106080d5dac85dee7885
Jonathan Hartman <jhartman86@gmail.com>
2015-02-23 01:30:56 -0700

    System dependencies and languages working on install

--- vagrant/provisioning/init.sh
+++ vagrant/provisioning/init.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+#####################################################################################
+# This file is modified slightly from the reference here:
+# http://akrabat.com/computing/provisioning-with-ansible-within-the-vagrant-guest/
+#####################################################################################
+
+export PYTHONUNBUFFERED=1
+
 if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
     echo "Add APT repositories"
@@ -14,5 +21,10 @@ then
     echo "Ansible installed"
 fi
 
+# Replace hosts file
+rm -f /etc/ansible/hosts
+printf 'localhost' >> /etc/ansible/hosts
+
+# Execute Ansible provisioner
 cd /vagrant/provisioning
-ansible-playbook setup.yml --connection=local
+ansible-playbook setup.yml --connection=local -vvv
