a5dcb993c271699acf1c42b281852a1f4677183b
Jonathan Hartman <jhartman86@gmail.com>
2015-02-22 17:53:09 -0700

    Provisioning

--- vagrant/vagrantfile.rb
+++ vagrant/vagrantfile.rb
@@ -2,8 +2,12 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+Vagrant.require_version ">= 1.6"
+
 Vagrant.configure("2") do |config|
 
-  
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.provision :shell, :keep_color => true, :inline => "export PYTHONUNBUFFERED=1 && export ANSIBLE_FORCE_COLOR=1 && cd /vagrant/provisioning && ./init.sh"
 
 end
