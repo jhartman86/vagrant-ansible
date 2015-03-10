f299ec0e1830b174b5b20a99bef8b0eb48788e8e
Jonathan Hartman <jhartman86@gmail.com>
2015-03-10 14:07:54 -0600

    Updates

--- provisioning/init.sh
+++ provisioning/init.sh
@@ -23,8 +23,14 @@ fi
 
 # Replace hosts file
 rm -f /etc/ansible/hosts
-printf 'localhost' >> /etc/ansible/hosts
+printf 'localhost ansible_ssh_host=127.0.0.1 ansible_ssh_user=vagrant ansible_ssh_pass=vagrant ansible_sudo_pass=vagrant' >> /etc/ansible/hosts
 
 # Execute Ansible provisioner
 cd /vagrant/provisioning
-ansible-playbook setup.yml --connection=local -vvvv
+
+# NOTE: if you run --connection=local flag, git clone fails in current ansible version,
+# so thats why we add all the bullshit info above to the hosts file. Since this is
+# for a dev machine, don't worry about the security; but DO.NOT. setup a hosts file
+# as above for production environments. Ever.
+#ansible-playbook setup.yml --connection=local -vvvv
+ansible-playbook setup.yml -vvvv
